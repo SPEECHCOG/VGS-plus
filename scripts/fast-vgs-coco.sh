19b31c6fbd325fa46dbaf81bbe59421f8d384d6b
khazarkhorrami <khazar.khorrami@gmail.com>
2022-09-26 10:32:24 +0300

    testing on juova

--- scripts/fast-vgs-coco.sh
+++ scripts/fast-vgs-coco.sh
@@ -14,8 +14,8 @@ python \
 --fb_w2v2_weights_fn ${fb_w2v2_weights_fn} \
 --exp_dir ${exp_dir} \
 --num_workers 4 \
---batch_size 32 \
---val_batch_size 32 \
+--batch_size 8 \
+--val_batch_size 8 \
 --val_cross_batch_size 8 \
 --n_epochs 50 \
 --n_print_steps 1000 \
