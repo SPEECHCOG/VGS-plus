c761f05c8db82af267a065f99914a18112b660ee
khazarkhorrami <khazar.khorrami@gmail.com>
2022-09-29 15:32:24 +0300

    ready to be tested on puhti

--- scripts/fast-vgs-coco.sh
+++ scripts/fast-vgs-coco.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source activate fastvgs
+source activate scratch/project_2001315/khazar_envs/myenvs/fastvgs 
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 data_root=$1
@@ -19,7 +19,7 @@ python \
 --val_cross_batch_size 8 \
 --n_epochs 50 \
 --n_print_steps 100 \
---n_val_steps 400 \
+--n_val_steps 100 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --xtrm_layers 1 \
