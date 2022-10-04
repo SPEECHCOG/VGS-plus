e73dcd580e9129064efb0b3f6ec0a0a48924fc41
khazarkhorrami <k.khazar@gmail.com>
2022-10-04 23:21:43 +0300

    model10b set

--- steps/trainer.py
+++ steps/trainer.py
@@ -772,9 +772,19 @@ class Trainer:
         #     self.args.coarse_matching_weight = 1
         #     self.args.caption_w2v2_weigh = 0.001
         # model 10
-        if n == 400000:
-            self.args.coarse_matching_weight = 1
-            self.args.caption_w2v2_weigh = 0.001
+        # if n == 200000:
+        #     self.args.coarse_matching_weight = 1
+        #     self.args.caption_w2v2_weigh = 0.001
+            
+        # model 10a
+        # if n == 400000:
+        #     self.args.coarse_matching_weight = 1
+        #     self.args.caption_w2v2_weigh = 0.001
+            
+        # model 10b (bs=64)
+        if n == 200000:
+            self.args.coarse_matching_weight = 0.1
+            self.args.caption_w2v2_weigh = 0.9
         # model 11
         # if n == 200000:
         #     self.args.coarse_matching_weight = 1
