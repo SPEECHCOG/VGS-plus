a590ce3cc58f1ef5ce9396f11a51412c24d3cb26
khazarkhorrami <k.khazar@gmail.com>
2022-10-07 13:48:27 +0300

    model 6 for puhti

--- scripts/fast-vgs-plus-coco.sh
+++ scripts/fast-vgs-plus-coco.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source activate fastvgs
+source activate /scratch/project_2001315/khazar_envs/myenvs/fastvgs
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 data_root=$1
@@ -15,19 +15,19 @@ python \
 --fb_w2v2_weights_fn ${fb_w2v2_weights_fn} \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
---batch_size 8 \
---val_batch_size 8 \
+--batch_size 32 \
+--val_batch_size 32 \
 --val_cross_batch_size 8 \
 --n_epochs 20 \
---n_print_steps 100 \
---n_val_steps 200000 \
+--n_print_steps 1000 \
+--n_val_steps 18505 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --normalize \
 --xtrm_layers 1 \
 --trm_layers 2 \
 --fine_matching_weight 0.0 \
---coarse_matching_weight 1 \
+--coarse_matching_weight 1.0 \
 --libri_w2v2_weight 0.0 \
 --caption_w2v2_weight 1.0 \
 --coarse_to_fine_retrieve \
