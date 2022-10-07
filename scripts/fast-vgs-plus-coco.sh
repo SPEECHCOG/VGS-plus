e8c7dcf0f3f7143f1b92c47acda9cca4bc3a643a
khazarkhorrami <k.khazar@gmail.com>
2022-10-07 11:55:09 +0300

    testing on juova for grad mul

--- scripts/fast-vgs-plus-coco.sh
+++ scripts/fast-vgs-plus-coco.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source activate /scratch/project_2001315/khazar_envs/myenvs/fastvgs
+source activate fastvgs
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 data_root=$1
@@ -16,22 +16,22 @@ python \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
 --resume \
---batch_size 32 \
---val_batch_size 32 \
+--batch_size 8 \
+--val_batch_size 8 \
 --val_cross_batch_size 8 \
---n_epochs 50 \
---n_print_steps 1000 \
---n_val_steps 18505 \
+--n_epochs 20 \
+--n_print_steps 100 \
+--n_val_steps 500 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --normalize \
 --xtrm_layers 1 \
 --trm_layers 6 \
 --fine_matching_weight 0.0 \
---coarse_matching_weight 0.001 \
+--coarse_matching_weight 1 \
 --libri_w2v2_weight 0.0 \
 --caption_w2v2_weight 1.0 \
 --coarse_to_fine_retrieve \
---feature_grad_mult 0. \
+--feature_grad_mult 1 \
 --layer_use 7
 
