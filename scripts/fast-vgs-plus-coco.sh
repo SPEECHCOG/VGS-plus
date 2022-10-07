2e69843031b59b4e377bf4e02b943b9ea51649bb
khazarkhorrami <hxkhkh@juovakivikyyhky.lin.tuni.fi>
2022-10-07 12:45:36 +0300

    testing on juovan

--- scripts/fast-vgs-plus-coco.sh
+++ scripts/fast-vgs-plus-coco.sh
@@ -15,23 +15,22 @@ python \
 --fb_w2v2_weights_fn ${fb_w2v2_weights_fn} \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
---resume \
 --batch_size 8 \
 --val_batch_size 8 \
 --val_cross_batch_size 8 \
 --n_epochs 20 \
 --n_print_steps 100 \
---n_val_steps 500 \
+--n_val_steps 5000 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --normalize \
 --xtrm_layers 1 \
---trm_layers 6 \
+--trm_layers 2 \
 --fine_matching_weight 0.0 \
 --coarse_matching_weight 1 \
 --libri_w2v2_weight 0.0 \
 --caption_w2v2_weight 1.0 \
 --coarse_to_fine_retrieve \
---feature_grad_mult 1 \
+--feature_grad_mult 0.1 \
 --layer_use 7
 
