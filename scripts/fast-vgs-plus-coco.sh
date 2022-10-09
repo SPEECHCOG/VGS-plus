ca6ab3668287df16d651ebcd5cdbe0d164889462
khazarkhorrami <k.khazar@gmail.com>
2022-10-09 18:10:08 +0300

    model6bTrim ready for puhti

--- scripts/fast-vgs-plus-coco.sh
+++ scripts/fast-vgs-plus-coco.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source activate fastvgs
+source activate /scratch/project_2001315/khazar_envs/myenvs/fastvgs
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 data_root=$1
@@ -25,15 +25,13 @@ python \
 --warmup_fraction 0.1 \
 --normalize \
 --xtrm_layers 1 \
---trm_layers 3 \
+--trm_layers 6 \
 --fine_matching_weight 0.0 \
 --coarse_matching_weight 1.0 \
 --libri_w2v2_weight 0.0 \
 --caption_w2v2_weight 1.0 \
 --coarse_to_fine_retrieve \
 --trim_mask \
---encoder_layers 6 \
---encoder_attention_heads 8 \
 --feature_grad_mult 0.1 \
---layer_use 5
+--layer_use 7
 
