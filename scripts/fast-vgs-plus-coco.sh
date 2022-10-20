f1dbee47f5b3ae24384a8fb34366fda584a03b8a
khazarkhorrami <khazar.khorrami@gmail.com>
2022-10-20 11:47:09 +0300

    script corrected for space mistake

--- scripts/fast-vgs-plus-coco.sh
+++ scripts/fast-vgs-plus-coco.sh
@@ -15,16 +15,16 @@ python \
 --fb_w2v2_weights_fn ${fb_w2v2_weights_fn} \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
---batch_size 8 \
---val_batch_size 8 \
+--batch_size 32 \
+--val_batch_size 32 \
 --val_cross_batch_size 8 \
 --n_epochs 24 \
---n_print_steps 500 \
+--n_print_steps 100 \
 --n_val_steps 9252 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --normalize \
---audio_feat_len 2.\
+--audio_feat_len 2.0 \
 --xtrm_layers 1 \
 --trm_layers 3 \
 --fine_matching_weight 0.0 \
@@ -36,5 +36,6 @@ python \
 --encoder_attention_heads 8 \
 --trim_mask \
 --feature_grad_mult 0.1 \
---layer_use 5
+--layer_use 5 \
+
 
