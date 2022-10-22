26504b0f32ce0e793e2a3ba9a310eaeeeafb7805
khazarkhorrami <hxkhkh@juovakivikyyhky.lin.tuni.fi>
2022-10-22 20:11:15 +0300

    preparing abx test shell

---
+++ run_ABX.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+source activate fastvgs
+
+python tester_ABX.py
+conda activate zerospeech2021
+zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o /worktmp/khorrami/current/ZeroSpeech/output/  -j12 --no-lexical --no-syntactic --no-semantic
+rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
