645b5ddf390346e067acde957e5e22af19730be5
khazarkhorrami <k.khazar@gmail.com>
2022-10-23 10:33:43 +0300

    cpu option

--- abx_19base3_E1L4.sh
+++ abx_19base3_E1L4.sh
@@ -6,5 +6,5 @@ python abx_19base3_E1L4.py
 mkdir /worktmp/khorrami/current/ZeroSpeech/output/abx_19base3_E1L4
 
 conda activate zerospeech2021
-zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o /worktmp/khorrami/current/ZeroSpeech/output/abx_19base3_E1L4  -j12 --no-lexical --no-syntactic --no-semantic
+zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o /worktmp/khorrami/current/ZeroSpeech/output/abx_19base3_E1L4  -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
 rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
