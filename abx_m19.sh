abd9cd32f901c7a5d5d95b1a3ce396af92c96bbc
khazarkhorrami <hxkhkh@juovakivikyyhky.lin.tuni.fi>
2022-10-23 22:08:10 +0300

    syntax error fixed

--- abx_m19.sh
+++ abx_m19.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 source activate fastvgs
 
-python abx_m19.py --mytarget_layer 2 --mytwd '/worktmp/khorrami/current/FaST/experiments/model19base1/138780_bundle.pth'
+python abx_m19.py --mytarget_layer 2 --mytwd '/worktmp/khorrami/current/FaST/experiments/model19base1/185040_bundle.pth'
 conda activate zerospeech2021
-mkdir /worktmp/khorrami/current/ZeroSpeech/output/m19base1E15L2
-zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o /worktmp/khorrami/current/ZeroSpeech/output/m19base1E15L2  -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
+mkdir /worktmp/khorrami/current/ZeroSpeech/output/m19base1E20L2
+zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o /worktmp/khorrami/current/ZeroSpeech/output/m19base1E20L2  -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
 rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
 
 conda activate fastvgs
-python abx_m19.py --mytarget_layer 3 --mytwd '/worktmp/khorrami/current/FaST/experiments/model19base1/138780_bundle.pth'
+python abx_m19.py --mytarget_layer 3 --mytwd '/worktmp/khorrami/current/FaST/experiments/model19base1/185040_bundle.pth'
 conda activate zerospeech2021
-mkdir /worktmp/khorrami/current/ZeroSpeech/output/m19base1E15L3
-zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o /worktmp/khorrami/current/ZeroSpeech/output/m19base1E15L3  -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
+mkdir /worktmp/khorrami/current/ZeroSpeech/output/m19base1E20L3
+zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o /worktmp/khorrami/current/ZeroSpeech/output/m19base1E20L3  -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
 rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
 
 conda activate fastvgs
-python abx_m19.py --mytarget_layer 4 --mytwd '/worktmp/khorrami/current/FaST/experiments/model19base1/138780_bundle.pth'
+python abx_m19.py --mytarget_layer 4 --mytwd '/worktmp/khorrami/current/FaST/experiments/model19base1/185040_bundle.pth'
 conda activate zerospeech2021
-mkdir /worktmp/khorrami/current/ZeroSpeech/output/m19base1E15L4
-zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o /worktmp/khorrami/current/ZeroSpeech/output/m19base1E15L4  -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
+mkdir /worktmp/khorrami/current/ZeroSpeech/output/m19base1E20L4
+zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o /worktmp/khorrami/current/ZeroSpeech/output/m19base1E20L4  -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
 rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
 
 conda activate fastvgs
-python abx_m19.py --mytarget_layer 5 --mytwd '/worktmp/khorrami/current/FaST/experiments/model19base1/138780_bundle.pth'
+python abx_m19.py --mytarget_layer 5 --mytwd '/worktmp/khorrami/current/FaST/experiments/model19base1/185040_bundle.pth'
 conda activate zerospeech2021
-mkdir /worktmp/khorrami/current/ZeroSpeech/output/m19base1E15L5
-zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o /worktmp/khorrami/current/ZeroSpeech/output/m19base1E15L5  -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
+mkdir /worktmp/khorrami/current/ZeroSpeech/output/m19base1E20L5
+zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o /worktmp/khorrami/current/ZeroSpeech/output/m19base1E20L5  -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
 rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
 
 
