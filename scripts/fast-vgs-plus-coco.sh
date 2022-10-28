dd251a0bfa41b642dcfc2877b09d1daa77f46e5a
khazarkhorrami <k.khazar@gmail.com>
2022-10-28 20:46:46 +0300

    m6base1T for puhti

--- scripts/fast-vgs-plus-coco.sh
+++ scripts/fast-vgs-plus-coco.sh
@@ -18,9 +18,9 @@ python \
 --batch_size 64 \
 --val_batch_size 100 \
 --val_cross_batch_size 8 \
---n_epochs 30 \
+--n_epochs 50 \
 --n_print_steps 500 \
---n_val_steps 9252 \
+--n_val_steps 46260 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --normalize \
