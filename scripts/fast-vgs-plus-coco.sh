8eb5a8380e4efdfe86a7e6050445b55ddc2c49fb
khazarkhorrami <k.khazar@gmail.com>
2022-10-28 21:08:57 +0300

    model6base3T ready for puhti

--- scripts/fast-vgs-plus-coco.sh
+++ scripts/fast-vgs-plus-coco.sh
@@ -25,11 +25,12 @@ python \
 --warmup_fraction 0.1 \
 --normalize \
 --xtrm_layers 1 \
---trm_layers 1 \
+--trm_layers 6 \
 --fine_matching_weight 0.0 \
 --coarse_matching_weight 1.0 \
 --libri_w2v2_weight 0.0 \
 --caption_w2v2_weight 1.0 \
 --feature_grad_mult 0.1 \
+--trim_mask \
 --layer_use 7 \
 
