9ae418d9100fee8f64a5f910c8e92ed0f2a149e1
khazarkhorrami <k.khazar@gmail.com>
2022-10-30 20:52:32 +0200

    bs to 100

--- scripts/fast-vgs-plus-coco.sh
+++ scripts/fast-vgs-plus-coco.sh
@@ -15,7 +15,7 @@ python \
 --fb_w2v2_weights_fn ${fb_w2v2_weights_fn} \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
---batch_size 128 \
+--batch_size 100 \
 --val_batch_size 100 \
 --val_cross_batch_size 8 \
 --n_epochs 50 \
