25fe38d6d6e8df210b54c05afc6e588522153371
khazarkhorrami <hxkhkh@juovakivikyyhky.lin.tuni.fi>
2022-11-08 10:56:53 +0200

    m7 b1 for all layers

--- abx_short_template.sh
+++ abx_short_template.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-NAME="model6base3T"
+NAME="model7base1T"
 OUTFOLDER="/worktmp/khorrami/current/ZeroSpeech/output"/$NAME
 mkdir $OUTFOLDER
 MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$NAME/"exp"
@@ -69,7 +69,7 @@ do
     rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
 done
 
-M="E45_bundle.pth"
+M="E40_bundle.pth"
 
 for LAYERNAME in 7 8 9 10 11
 do
@@ -83,4 +83,18 @@ do
     rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
 done
 
+
+M="E45_bundle.pth"
+
+for LAYERNAME in 1 2 3 4 5 6 7 8 9 10 11
+do
+    OUTNAME="E45L"$LAYERNAME
+    OUTFILE=$OUTFOLDER/$OUTNAME
+    conda activate fastvgs
+    python abx.py --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
+    conda activate zerospeech2021
+    mkdir $OUTFILE
+    zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o $OUTFILE  -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
+    rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
+done
 
