d636bb55295cbfecf04a895ab1065adbdc10b954
khazarkhorrami <k.khazar@gmail.com>
2022-11-09 19:26:36 +0200

    ready to run

--- scripts/fast-vgs-plus-coco.sh
+++ scripts/fast-vgs-plus-coco.sh
@@ -15,9 +15,9 @@ python \
 --fb_w2v2_weights_fn ${fb_w2v2_weights_fn} \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
---batch_size 2 \
+--batch_size 64 \
 --val_batch_size 100 \
---val_cross_batch_size 8 \
+--val_cross_batch_size 100 \
 --n_epochs 50 \
 --n_print_steps 500 \
 --n_val_steps 46260 \
