d003b4a6f9b82d2887715e06846d0fca639630ed
khazarkhorrami <k.khazar@gmail.com>
2022-11-20 23:29:43 +0200

    model7ver4 resume puhti

--- scripts/fast-vgs-plus-coco.sh
+++ scripts/fast-vgs-plus-coco.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source activate fastvgs
+source activate /scratch/project_2001315/khazar_envs/myenvs/fastvgs
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 data_root=$1
@@ -7,6 +7,7 @@ raw_audio_base_path=$2
 fb_w2v2_weights_fn=$3
 exp_dir=$4
 libri_fn_root=$5
+twd=$6
 
 python \
 ../run_spokencoco.py \
@@ -15,25 +16,24 @@ python \
 --fb_w2v2_weights_fn ${fb_w2v2_weights_fn} \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
+--trained_weights_dir ${twd} \
 --resume \
 --batch_size 64 \
 --val_batch_size 100 \
 --val_cross_batch_size 100 \
---n_epochs 80 \
+--n_epochs 50 \
 --n_print_steps 500 \
 --n_val_steps 46260 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --normalize \
 --xtrm_layers 1 \
---trm_layers 3 \
+--trm_layers 6 \
 --fine_matching_weight 0.0 \
 --coarse_matching_weight 1.0 \
 --libri_w2v2_weight 0.0 \
 --caption_w2v2_weight 1.0 \
 --feature_grad_mult 1.0 \
---encoder_layers 6 \
---encoder_attention_heads 8 \
 --trim_mask \
---layer_use 5 \
+--layer_use 7 \
 
