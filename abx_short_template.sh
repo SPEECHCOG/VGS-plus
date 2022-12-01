d6fa89ff6851e8ea6ab5ac8f1fa50a38a852b082
khazarkhorrami <k.khazar@gmail.com>
2022-12-01 22:49:23 +0200

    new changes for plot

--- abx_short_template.sh
+++ abx_short_template.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-NAME="model7ver6"
+NAME="model7ver7"
 OUTFOLDER="/worktmp/khorrami/current/ZeroSpeech/output"/$NAME
 mkdir $OUTFOLDER
 MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$NAME/"exp"
@@ -54,23 +54,10 @@ do
 done
 
 
-M="E35_bundle.pth"
+M="E30_bundle.pth"
 for LAYERNAME in 1 2 3 4 5 6 7 8
 do
-    OUTNAME="E35L"$LAYERNAME
-    OUTFILE=$OUTFOLDER/$OUTNAME
-    conda activate fastvgs
-    python abx.py --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
-    conda activate zerospeech2021
-    mkdir $OUTFILE
-    zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
-    rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
-done
-
-M="E45_bundle.pth"
-for LAYERNAME in 1 2 3 4 5 6 7 8
-do
-    OUTNAME="E45L"$LAYERNAME
+    OUTNAME="E30L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
     conda activate fastvgs
     python abx.py --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
