6c7ab0495f4c75d9f4d0c91bba75a67bbdb1bf1c
khazarkhorrami <k.khazar@gmail.com>
2022-12-02 16:11:43 +0200

    model7 for new test

--- scripts/fast-vgs-plus-coco.sh
+++ scripts/fast-vgs-plus-coco.sh
@@ -7,7 +7,6 @@ raw_audio_base_path=$2
 fb_w2v2_weights_fn=$3
 exp_dir=$4
 libri_fn_root=$5
-twd=$6
 
 python \
 ../run_spokencoco.py \
@@ -16,13 +15,12 @@ python \
 --fb_w2v2_weights_fn ${fb_w2v2_weights_fn} \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
---trained_weights_dir ${twd} \
 --batch_size 64 \
 --val_batch_size 100 \
 --val_cross_batch_size 100 \
---n_epochs 50 \
+--n_epochs 10 \
 --n_print_steps 500 \
---n_val_steps 46260 \
+--n_val_steps 9252 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --normalize \
