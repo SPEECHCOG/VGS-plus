804559dca0ad7570eb969adc97feca8b28fe145e
khazarkhorrami <khazar.khorrami@gmail.com>
2022-12-08 20:50:06 +0200

    abx ver0

--- abx_short_template.sh
+++ abx_short_template.sh
@@ -4,9 +4,9 @@ OUTFOLDER="/worktmp/khorrami/current/ZeroSpeech/output"/$NAME
 mkdir $OUTFOLDER
 MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$NAME/"exp"
 
-M="E6_bundle.pth"
+M="E0_bundle.pth"
 
-OUTNAME="E6L1"
+OUTNAME="E0L1"
 OUTFILE=$OUTFOLDER/$OUTNAME
 source activate fastvgs
 python abx.py --mytarget_layer 1 --mytwd $MFOLDER/$M
@@ -17,33 +17,7 @@ rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
 
 for LAYERNAME in 2 3 4 5 6 7 8
 do
-    OUTNAME="E6L"$LAYERNAME
-    OUTFILE=$OUTFOLDER/$OUTNAME
-    conda activate fastvgs
-    python abx.py --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
-    conda activate zerospeech2021
-    mkdir $OUTFILE
-    zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
-    rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
-done
-
-M="E7_bundle.pth"
-for LAYERNAME in 1 2 3 4 5 6 7 8
-do
-    OUTNAME="E7L"$LAYERNAME
-    OUTFILE=$OUTFOLDER/$OUTNAME
-    conda activate fastvgs
-    python abx.py --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
-    conda activate zerospeech2021
-    mkdir $OUTFILE
-    zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
-    rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
-done
-
-M="E8_bundle.pth"
-for LAYERNAME in 1 2 3 4 5 6 7 8
-do
-    OUTNAME="E8L"$LAYERNAME
+    OUTNAME="E0L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
     conda activate fastvgs
     python abx.py --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
@@ -54,7 +28,7 @@ do
 done
 
 M="E9_bundle.pth"
-for LAYERNAME in 1 2 3 4 5 6 7 8
+for LAYERNAME in 7 8
 do
     OUTNAME="E9L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
@@ -65,6 +39,7 @@ do
     zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
     rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
 done
+
 
 M="E10_bundle.pth"
 for LAYERNAME in 1 2 3 4 5 6 7 8
