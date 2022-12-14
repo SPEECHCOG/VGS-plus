d4701619b8902dc83e6e6a6b55f9c3ff56154fec
khazarkhorrami <k.khazar@gmail.com>
2022-12-14 16:04:15 +0200

    plot norm graphs

--- plot-cogsci.py
+++ plot-cogsci.py
@@ -279,7 +279,7 @@ plt.plot(m7base3[1], label='layer6')
 plt.plot(m7base3[2], label='layer7')
 plt.title('VGS+ (random init), 5 epochs ',size=14)  
 plt.ylabel('abx-error', size=18) 
-plt.xticks([0,1,2,3,4],['1','2','3','4','5'])
+plt.xticks([1,2,3,4,5],['1','2','3','4','5'])
 plt.grid()
 plt.legend(fontsize=14) 
 
@@ -289,7 +289,7 @@ plt.plot(m7base3T[1], label='layer6')
 plt.plot(m7base3T[2], label='layer7')
 plt.title('VGS+ (random init), 50 epochs ',size=14)  
 plt.ylabel('abx-error', size=18) 
-plt.xticks([0,1,2,3,4,5,6,7],['5', '15', '25' ,'35','45','55','65','75'])
+plt.xticks([1,2,3,4,5,6,7,8],['5', '15', '25' ,'35','45','55','65','75'])
 plt.grid()
 plt.legend(fontsize=14) 
 
@@ -331,7 +331,7 @@ plt.savefig(os.path.join(path_save, 'ABX-original' + '.png'), format='png')
 ############################################################################## lexical
 path_input = "/worktmp2/hxkhkh/current/lextest/output/"
 ##################################################################
-                        ### m7base1T  ###
+                        ### m7base1T  ### 
 ################################################################## 
 scoresF_m7base1 = []
 model_name = 'model7base1T'
@@ -635,146 +635,165 @@ plt.savefig(os.path.join(path_save, 'lexical' + '.png'), format='png')
 
 
 # =============================================================================
-# ########################################## Normalizing recalls
+# ########################################## Normalizing and merging recalls
 # =============================================================================
-max_recall_3 = np.max(y_7base3T_recall)
+x_recall_m7base3 = []
+y_recall_m7base3 = []
+
+x_recall_m7base3.extend(x_7base3_recall[0:6])
+y_recall_m7base3.extend(y_7base3_recall[0:6])
+
+x_recall_m7base3.extend(x_7base3T_recall[2:12])
+y_recall_m7base3.extend(y_7base3T_recall[2:12])
+
+
+x_recall_m7ver4 = []
+y_recall_m7ver4 = []
+
+x_recall_m7ver4.extend(x_7ver4_recall[0:6])
+y_recall_m7ver4.extend(y_7ver4_recall[0:6])
+
+x_recall_m7ver4.extend(x_7ver4T_recall[2:12])
+y_recall_m7ver4.extend(y_7ver4T_recall[2:12])
+
+max_recall_3 = np.max(y_recall_m7base3)
 max_recall_4 = np.max(y_7ver4T_recall)
 
-min_recall_3 = np.min(y_7base3T_recall)
+min_recall_3 = np.min(y_recall_m7base3)
 min_recall_4 = np.min(y_7ver4T_recall)
 
 delta_recall_3 = max_recall_3 - min_recall_3
 delta_recall_4 = max_recall_4 - min_recall_4
 
-r_3T = [(item - min_recall_3) / delta_recall_3 for item in y_7base3T_recall]
-r_4T = [(item - min_recall_4) / delta_recall_4 for item in y_7ver4T_recall]
-
-r_3 = [(item - min_recall_3) / delta_recall_3 for item in y_7base3_recall]
-r_4 = [(item - min_recall_4) / delta_recall_4 for item in y_7ver4_recall]
+r_3 = [(item - min_recall_3) / delta_recall_3 for item in y_recall_m7base3]
+r_4 = [(item - min_recall_4) / delta_recall_4 for item in y_recall_m7ver4]
 
 # =============================================================================
-# ############################################## Normalizing ABX
+# ############################################## Normalizing and merging ABX
 # =============================================================================
-m7base3_selected = np.min(m7base3 , axis = 0)
-m7base3T_selected = np.min(m7base3T , axis = 0)
-m7ver4_selected = np.min(m7ver4 , axis = 0)
-m7ver4T_selected = np.min(m7ver4T , axis = 0)
+# base3: ['1','2','3','4','5']['5', '15', '25' ,'35','45','55','65','75']
+# ver4: ['0','1','2','3','4','5']['0','5', '15', '25' ,'35','45']
 
-m7base3T_selected = np.insert(m7base3T_selected,0, 50)
+x_abx_m7base3 = [0,1,2,3,4,5,5,15,25,35,45,55,65,75]
+y_abx_m7base3 = []
 
+y_abx_m7base3 = []
+y_abx_m7base3.extend(np.min(m7base3 , axis = 0)) #best layer performance
+y_abx_m7base3.extend(np.min(m7base3T , axis = 0)) #best layer performance
+y_abx_m7base3.insert(0, 50)
 
-max_abx_3 = 50 #np.max(m7base3_selected)
-max_abx_4 = 50 #np.max(m7ver4_selected)
+x_abx_m7ver4 = [0,1,2,3,4,5,5,15,25,35,45]
+y_abx_m7ver4 = []
 
-min_abx_3 = np.min(m7base3T_selected)
-min_abx_4 = np.min(m7ver4T_selected)
+y_abx_m7ver4 = []
+y_abx_m7ver4.extend(np.min(m7ver4 , axis = 0)) #best layer performance
+y_abx_m7ver4.extend(np.min(m7ver4T[:,1:] , axis = 0)) #best layer performance
+
+plt.plot(y_abx_m7ver4)
+
+max_abx_3 = np.max(y_abx_m7base3)
+max_abx_4 = np.max(y_abx_m7ver4)
+
+min_abx_3 = np.min(y_abx_m7base3)
+min_abx_4 = np.min(y_abx_m7ver4)
 
 delta_abx_3 = max_abx_3 - min_abx_3
 delta_abx_4 = max_abx_4 - min_abx_4
 
-abx_3T = [1- ((item - min_abx_3) / delta_abx_3) for item in m7base3T_selected]
-abx_4T = [1- ((item - min_abx_4) / delta_abx_4) for item in m7ver4T_selected]
-
-abx_3 = [1- ((item - min_abx_3) / delta_abx_3) for item in m7base3_selected]
-abx_4 = [1- ((item - min_abx_4) / delta_abx_4) for item in m7ver4_selected]
-
-# =============================================================================
-# ############################################## Normalizing Lexical C
-# =============================================================================
-m7Cbase3_selected = np.max(m7Cbase3 , axis = 0)
-m7Cbase3T_selected = np.max(m7Cbase3T , axis = 0)
-m7Cver4_selected = np.max(m7Cver4 , axis = 0)
-
-m7Cbase3T_selected = np.insert(m7Cbase3T_selected,0, 1/89)
-
-
-max_lexC_3 = np.max(m7Cbase3_selected)
-max_lexC_4 = np.max(m7Cver4_selected)
-
-min_lexC_3 = 1/89 # np.min(m7base3T_selected)
-min_lexC_4 = 1/89 #np.min(m7ver4T_selected)
-
-delta_lexC_3 = max_lexC_3 - min_lexC_3
-delta_lexC_4 = max_lexC_4 - min_lexC_4
-
-lexC_3T = [(item - min_lexC_3) / delta_lexC_3 for item in m7Cbase3T_selected]
-lexC_3 = [(item - min_lexC_3) / delta_lexC_3 for item in m7Cbase3_selected]
-lexC_4 = [(item - min_lexC_4) / delta_lexC_4 for item in m7Cver4_selected]
+abx_3 = [1- ((item - min_abx_3) / delta_abx_3) for item in y_abx_m7base3]
+abx_4 = [1- ((item - min_abx_4) / delta_abx_4) for item in y_abx_m7ver4]
 
 # =============================================================================
 # ############################################## Normalizing Lexical F
 # =============================================================================
-m7Fbase3_selected = np.max(m7Fbase3 , axis = 0)
-m7Fbase3T_selected = np.max(m7Fbase3T , axis = 0)
-m7Fver4_selected = np.max(m7Fver4 , axis = 0)
+# base3: ['1','2','3','4','5']['5', '15', '25','35','45']
+# ver4: ['1','2','3','4','5']['5', '15', '25','35','45']
 
-m7Fbase3T_selected = np.insert(m7Fbase3T_selected,0, 1/89)
+x_lexF_m7base3 = [0,1,2,3,4,5,15,25,35,45]
+y_lexF_m7base3 = []
+y_lexF_m7base3.extend(np.max(m7Fbase3 , axis = 0)) #best layer performance
+y_lexF_m7base3.extend(np.max(m7Fbase3T[:,1:] , axis = 0)) #best layer performance
+y_lexF_m7base3.insert(0, 1/89)
 
+x_lexF_m7ver4 = [0,1,2,3,4,5,15,25,35,45]
+y_lexF_m7ver4 = []
 
-max_lexF_3 = np.max(m7Fbase3_selected)
-max_lexF_4 = np.max(m7Fver4_selected)
+y_lexF_m7ver4 = []
+y_lexF_m7ver4.extend(np.max(m7Fver4 , axis = 0)) #best layer performance
+bestF_w2v2 = 0.268 
+y_lexF_m7ver4.insert(0, bestF_w2v2)
 
-min_lexF_3 = 1/89 # np.min(m7base3T_selected)
-min_lexF_4 = 1/89 #np.min(m7ver4T_selected)
+max_lexF_3 = np.max(y_lexF_m7base3)
+max_lexF_4 = np.max(y_lexF_m7ver4)
+
+min_lexF_3 = np.min(y_lexF_m7base3)
+min_lexF_4 = np.min(y_lexF_m7ver4)
 
 delta_lexF_3 = max_lexF_3 - min_lexF_3
 delta_lexF_4 = max_lexF_4 - min_lexF_4
 
-lexF_3T = [(item - min_lexF_3) / delta_lexF_3 for item in m7Fbase3T_selected]
-lexF_3 = [(item - min_lexF_3) / delta_lexF_3 for item in m7Fbase3_selected]
-lexF_4 = [(item - min_lexF_4) / delta_lexF_4 for item in m7Fver4_selected]
+lexF_3 = [(item - min_lexF_3) / delta_lexF_3 for item in y_lexF_m7base3]
+lexF_4 = [(item - min_lexF_4) / delta_lexF_4 for item in y_lexF_m7ver4]
+
+# =============================================================================
+# ############################################## Normalizing Lexical C : bestC_w2v2 = 0.383
+# =============================================================================
+
+# base3: ['1','2','3','4','5']['5', '15', '25','35','45']
+# ver4: ['1','2','3','4','5']['5', '15', '25','35','45']
+
+x_lexC_m7base3 = [0,1,2,3,4,5,15,25,35,45]
+y_lexC_m7base3 = []
+y_lexC_m7base3.extend(np.max(m7Cbase3 , axis = 0)) #best layer performance
+y_lexC_m7base3.extend(np.max(m7Cbase3T[:,1:] , axis = 0)) #best layer performance
+y_lexC_m7base3.insert(0, 1/89)
+
+x_lexC_m7ver4 = [0,1,2,3,4,5,15,25,35,45]
+y_lexC_m7ver4 = []
+
+y_lexC_m7ver4 = []
+y_lexC_m7ver4.extend(np.max(m7Cver4 , axis = 0)) #best layer performance
+bestC_w2v2 = 0.383 
+y_lexC_m7ver4.insert(0, bestC_w2v2)
+
+max_lexC_3 = np.max(y_lexC_m7base3)
+max_lexC_4 = np.max(y_lexC_m7ver4)
+
+min_lexC_3 = np.min(y_lexC_m7base3)
+min_lexC_4 = np.min(y_lexC_m7ver4)
+
+delta_lexC_3 = max_lexC_3 - min_lexC_3
+delta_lexC_4 = max_lexC_4 - min_lexC_4
+
+lexC_3 = [(item - min_lexC_3) / delta_lexC_3 for item in y_lexC_m7base3]
+lexC_4 = [(item - min_lexC_4) / delta_lexC_4 for item in y_lexC_m7ver4]
 ########################################### Plotting Normalized graphs
 
 fig = plt.figure(figsize=(10,10))
 
-title = 'VGS+ (random init), 5 epochs '
-plt.subplot(2,2,1)
-plt.plot(x_7base3_recall[0:6], r_3[0:6], c_1, label = 'recall@10')
-plt.plot(abx_3,c_3, label='abx')
-plt.plot(lexC_3,c_2, label='lexC')
-plt.plot(lexF_3,c_4, label='lexF')
+title = 'VGS+ (random init) '
+plt.subplot(2,1,1)
+plt.plot(x_recall_m7base3, r_3, c_1, label = 'recall@10')
+plt.plot(x_abx_m7base3[0:-3],abx_3[0:-3],c_3, label='abx')
+plt.plot(x_lexC_m7base3, lexC_3,c_2, label='lex-average')
+plt.plot(x_lexF_m7base3, lexF_3,c_4, label='lex-frame')
 plt.ylim(0,1)
 plt.ylabel('Normalized performance',size=18)
-plt.xticks([0,1,2,3,4],['1','2','3','4','5'])
+plt.xticks([0,1,2,3,4,5,10,15,20,25,30,35,40,45,50],['0','1','2','3','4','5','10','15','20','25','30','35','40','45','50'])
 plt.grid()
 plt.legend(fontsize=12)
 plt.title(title)
 
-title = 'VGS+ (random init), 50 epochs '
-plt.subplot(2,2,2)
-plt.plot(x_7base3T_recall[:-6], r_3T[:-6], c_1, label = 'recall@10')
-plt.plot([0,5,15,25,35,45,55], abx_3T[:7],c_3, label='abx')
-plt.plot([0,5,15,25,35,45],lexC_3T,c_2, label='lexC')
-plt.plot([0,5,15,25,35,45],lexF_3T,c_4, label='lexF')
+title = 'VGS+ (Pretrained) '
+plt.subplot(2,1,2)
+plt.plot(x_recall_m7ver4, r_3, c_1, label = 'recall@10')
+plt.plot(x_abx_m7ver4, abx_4,c_3, label='abx')
+plt.plot(x_lexC_m7ver4, lexC_4,c_2, label='lex-average')
+plt.plot(x_lexF_m7ver4, lexF_4,c_4, label='lex-frame')
+
 plt.ylim(0,1)
-plt.xticks([0,5,15,25,35,45,55],['0','5','15','25','35','45','55'])
-plt.grid()
-plt.legend(fontsize=12)
-plt.title(title)
-
-title = 'VGS+ (Pretrained), 5 epochs '
-plt.subplot(2,2,3)
-plt.plot(x_7ver4_recall[0:6], r_4[0:6], c_1, label = 'recall@10')
-plt.plot(abx_4,c_3, label='abx')
-plt.plot(lexC_4[0:5],c_2, label='lexC')
-plt.plot(lexF_4[0:5],c_4, label='lexF')
-plt.xlabel('epoch',size=18)
 plt.ylabel('Normalized performance',size=18)
-plt.ylim(0,1)
-plt.xticks([0,1,2,3,4],['1','2','3','4','5'])
-plt.grid()
-plt.legend(fontsize=12)
-plt.title(title)
-
-title = 'VGS+ (Pretrained), 50 epochs '
-plt.subplot(2,2,4)
-plt.plot(x_7ver4T_recall, r_4T, c_1, label = 'recall@10')
-plt.plot([0,5,15,25,35,45],abx_4T,c_3, label='abx')
-plt.plot([5,15,25,35,45],lexC_4[4:],c_2, label='lexC')
-plt.plot([5,15,25,35,45],lexF_4[4:],c_4, label='lexF')
-plt.xlabel('epoch',size=18)
-plt.ylim(0,1)
-plt.xticks([0,5,15,25,35,45],['0','5','15','25','35','45'])
+plt.xticks([0,1,2,3,4,5,10,15,20,25,30,35,40,45,50],['0','1','2','3','4','5','10','15','20','25','30','35','40','45','50'])
 plt.grid()
 plt.legend(fontsize=12)
 plt.title(title)
