f2f55f59cadb060b8d158f6d6302282e6d5f39ee
khazarkhorrami <hxkhkh@juovakivikyyhky.lin.tuni.fi>
2023-01-19 10:24:42 +0200

    complete set of base3

--- abx_long_template.sh
+++ abx_long_template.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-NAME="model7ver5"
+NAME="model7base3T"
 OUTFOLDER="/worktmp/khorrami/current/ZeroSpeech/output"/$NAME
 mkdir $OUTFOLDER
 MFOLDER="/worktmp/khorrami/current/FaST/experiments"/$NAME/"exp"
 
-M="E5_bundle.pth"
+M="E10_bundle.pth"
 
-OUTNAME="E5L1"
+OUTNAME="E10L1"
 OUTFILE=$OUTFOLDER/$OUTNAME
 source activate fastvgs
 python abx.py --mytarget_layer 1 --mytwd $MFOLDER/$M
@@ -17,7 +17,7 @@ rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
 
 for LAYERNAME in 2 3 4 5 6 7 8
 do
-    OUTNAME="E5L"$LAYERNAME
+    OUTNAME="E10L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
     conda activate fastvgs
     python abx.py --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
@@ -27,8 +27,22 @@ do
     rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
 done
 
-M="E15_bundle.pth"
+M="E20_bundle.pth"
 for LAYERNAME in 1 2 3 4 5 6 7 8
+do
+    OUTNAME="E20L"$LAYERNAME
+    OUTFILE=$OUTFOLDER/$OUTNAME
+    conda activate fastvgs
+    python abx.py --mytarget_layer $LAYERNAME --mytwd $MFOLDER/$M
+    conda activate zerospeech2021
+    mkdir $OUTFILE
+    zerospeech2021-evaluate /worktmp/khorrami/current/ZeroSpeech/data/  /worktmp/khorrami/current/ZeroSpeech/submission/ -o $OUTFILE -j12 --no-lexical --no-syntactic --no-semantic --force-cpu
+    rm -r /worktmp/khorrami/current/ZeroSpeech/submission/phonetic/dev-clean
+done
+
+
+M="E15_bundle.pth"
+for LAYERNAME in 1 2 3 4 8
 do
     OUTNAME="E15L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
@@ -42,7 +56,7 @@ done
 
 
 M="E25_bundle.pth"
-for LAYERNAME in 1 2 3 4 5 6 7 8
+for LAYERNAME in 1 2 3 4 8
 do
     OUTNAME="E25L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
@@ -55,7 +69,7 @@ do
 done
 
 M="E35_bundle.pth"
-for LAYERNAME in 1 2 3 4 5 6 7 8
+for LAYERNAME in 1 2 3 4 8
 do
     OUTNAME="E35L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
@@ -68,7 +82,7 @@ do
 done
 
 M="E45_bundle.pth"
-for LAYERNAME in 1 2 3 4 5 6 7 8
+for LAYERNAME in 1 2 3 4 8
 do
     OUTNAME="E45L"$LAYERNAME
     OUTFILE=$OUTFOLDER/$OUTNAME
