1b37f0695de8f7885fcdd6268d9e1a3cbf18165e
khazarkhorrami <k.khazar@gmail.com>
2023-01-27 12:11:35 +0200

    minor changes

--- read_lex.py
+++ read_lex.py
@@ -44,7 +44,7 @@ def read_score (path):
 scores_m7base1 = []
 model_name = 'model7base1T'
 layer_names = ['L1','L2','L3','L4','L5','L6','L7','L8']
-for epoch in [5,10,15,25,35,45]:
+for epoch in [5,10,15,20,25,35,45]:
     print(epoch)
     for layer_name in layer_names:
         name = 'E' + str(epoch) + layer_name
@@ -54,14 +54,14 @@ for epoch in [5,10,15,25,35,45]:
         s = read_score (path)
         scores_m7base1.append(s)
 
-m7base1 = (np.reshape(scores_m7base1, (6,8))).T
+m7base1 = (np.reshape(scores_m7base1, (7,8))).T
 ##################################################################
                         ### model7base3T  ###
 ################################################################## 
 scores_m7base3T = []
 model_name = 'model7base3T'
 layer_names = ['L1','L2','L3','L4','L5','L6','L7','L8']
-for epoch in [1,2,3,4,5,15,25, 35, 45]:
+for epoch in [1,2,3,4,5,10,15,20,25, 35, 45]:
     print(epoch)
     for layer_name in layer_names:
         name = 'E' + str(epoch) + layer_name
@@ -71,7 +71,7 @@ for epoch in [1,2,3,4,5,15,25, 35, 45]:
         s = read_score (path)
         scores_m7base3T.append(s)
 
-m7base3T = (np.reshape(scores_m7base3T, (9,8))).T
+m7base3T = (np.reshape(scores_m7base3T, (11,8))).T
 
 ##################################################################
                         ### model7ver4T  ###
@@ -92,6 +92,25 @@ for epoch in [1,2,3,4,5,15,25, 35, 45]:
 
 m7ver4T = (np.reshape(scores_m7ver4T, (9,8))).T
 
+##################################################################
+                        ### model7ver8T  ###
+################################################################## 
+scores_m7ver8T = []
+model_name = 'model7ver8'
+layer_names = ['L1','L2','L3','L4','L5','L6','L7','L8']
+for epoch in [1,2,3,4,5,15,25, 35, 45]:
+    print(epoch)
+    for layer_name in layer_names:
+        name = 'E' + str(epoch) + layer_name
+        print(name) # name = 'E10L3'
+        path = os.path.join(path_input, 'cls', model_name , name , 'output.txt')
+        name = 'E' + str(epoch) + layer_name
+        s = read_score (path)
+        scores_m7ver8T.append(s)
+
+
+m7ver8T = (np.reshape(scores_m7ver8T, (9,8))).T
+kh
 ##################################################################
                         ### m7base4T  ###
 ################################################################## 
@@ -132,12 +151,12 @@ m7base5 = (np.reshape(scores_m7base5, (9,8))).T
 # ############################################# Normalizing and merging Lexcical
 # =============================================================================
 
-x_abx_m7base1 = [0.,5,10,15,25,35,45]
+x_abx_m7base1 = [0.,5,10,15,20,25,35,45]
 y_abx_m7base1 = []
 y_abx_m7base1.extend(np.max(m7base1 , axis = 0)) #best layer performance
 y_abx_m7base1.insert(0, 1/89) 
 
-x_abx_m7base3 = [0.,1,2,3,4,5,15,25,35,45]
+x_abx_m7base3 = [0.,1,2,3,4,5,10,15,20,25,35,45]
 y_abx_m7base3 = []
 y_abx_m7base3.extend(np.max(m7base3T , axis = 0)) #best layer performance
 y_abx_m7base3.insert(0, 1/89) 
@@ -145,8 +164,14 @@ y_abx_m7base3.insert(0, 1/89)
 x_abx_m7ver4 = [0.,1,2,3,4,5,15,25,35,45]
 y_abx_m7ver4 = []
 y_abx_m7ver4.extend(np.max(m7ver4T , axis = 0)) #best layer performance
-bestC_w2v2 = 0.364 
+bestC_w2v2 = 0.294 
 y_abx_m7ver4.insert(0, bestC_w2v2)
+
+x_abx_m7ver8 = [0.,1,2,3,4,5,15,25,35,45]
+y_abx_m7ver8 = []
+y_abx_m7ver8.extend(np.max(m7ver8T , axis = 0)) #best layer performance
+bestC_VGSplus = 0.584
+y_abx_m7ver8.insert(0, bestC_VGSplus)
 
 
 x_abx_m7base4 = [0.,1,2,3,4,5,15,25,35,45]
@@ -167,6 +192,7 @@ delta_abx = max_abx - min_abx
 abx_b1 = [ ((item - min_abx) / delta_abx) for item in y_abx_m7base1]
 abx_b3 = [ ((item - min_abx) / delta_abx) for item in y_abx_m7base3]
 abx_v4 = [ ((item - min_abx) / delta_abx) for item in y_abx_m7ver4]
+abx_v8 = [ ((item - min_abx) / delta_abx) for item in y_abx_m7ver8]
 abx_b4 = [ ((item - min_abx) / delta_abx) for item in y_abx_m7base4]
 abx_b5 = [ ((item - min_abx) / delta_abx) for item in y_abx_m7base5]
 
@@ -174,15 +200,17 @@ abx_b5 = [ ((item - min_abx) / delta_abx) for item in y_abx_m7base5]
 
 x_abx_m7base3 [0] = 0.5
 x_abx_m7ver4 [0] = 0.5
+x_abx_m7ver8 [0] = 0.5
 x_abx_m7base4 [0] = 0.5
 x_abx_m7base5 [0] = 0.5
 
 fig = plt.figure(figsize=(8,8))
 ax = fig.add_subplot(2, 1, 1)
 plt.plot(x_abx_m7base3, abx_b3 ,c_1 , label='VGS+ (0.5)')
-plt.plot(x_abx_m7ver4, abx_v4 ,c_2 , label='VGS+ Pre')
-plt.plot(x_abx_m7base4, abx_b4 ,c_3 , label='VGS+ (0.1)')
-plt.plot(x_abx_m7base5, abx_b5 ,c_4 , label='VGS+ (0.9)')
+plt.plot(x_abx_m7ver4, abx_v4 ,c_2 , label='VGS+ Pre with w2v2')
+plt.plot(x_abx_m7ver8, abx_v8 ,c_3 , label='w2v2 Pre VGS+')
+plt.plot(x_abx_m7base4, abx_b4 ,c_4 , label='VGS+ (0.1)')
+plt.plot(x_abx_m7base5, abx_b5 ,c_5 , label='VGS+ (0.9)')
 ax.set_xscale('log')
 plt.xticks([0.5,1,2,3,4,5,10,50],['0','1','2','3','4','5','10','50'])
 plt.ylim(0,1)
@@ -197,6 +225,7 @@ from scipy.io import savemat
 
 x_abx_m7base3 [0] = 0.
 x_abx_m7ver4 [0] = 0.
+x_abx_m7ver8 [0] = 0.
 x_abx_m7base4 [0] = 0.
 x_abx_m7base5 [0] = 0.
 
@@ -214,10 +243,16 @@ dict_recall['VGSplus05']['y'] = y_abx_m7base3
 dict_recall['VGSplus05']['norm'] = abx_b3
 
 
-dict_recall['VGSpluspre'] = {}
-dict_recall['VGSpluspre']['x'] = x_abx_m7ver4
-dict_recall['VGSpluspre']['y'] = y_abx_m7ver4
-dict_recall['VGSpluspre']['norm'] = abx_v4
+dict_recall['VGSplusprew2v2'] = {}
+dict_recall['VGSplusprew2v2']['x'] = x_abx_m7ver4
+dict_recall['VGSplusprew2v2']['y'] = y_abx_m7ver4
+dict_recall['VGSplusprew2v2']['norm'] = abx_v4
+
+
+dict_recall['w2v2preVGSplus'] = {}
+dict_recall['w2v2preVGSplus']['x'] = x_abx_m7ver8
+dict_recall['w2v2preVGSplus']['y'] = y_abx_m7ver8
+dict_recall['w2v2preVGSplus']['norm'] = abx_v8
 
 dict_recall['VGSplus01'] = {}
 dict_recall['VGSplus01']['x'] = x_abx_m7base4
@@ -229,4 +264,4 @@ dict_recall['VGSplus09']['x'] = x_abx_m7base5
 dict_recall['VGSplus09']['y'] = y_abx_m7base5
 dict_recall['VGSplus09']['norm'] = abx_b5
 
-#savemat(path_save + "lex.mat", dict_recall)
+savemat(path_save + "lex.mat", dict_recall)
