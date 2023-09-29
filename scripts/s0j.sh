ea729c1d5beceb1f6d9710cf15660cbbf5ee5172
Khazar Khorrami <hxkhkh@mustaotsakolibri.lin.tuni.fi>
2023-09-29 15:40:34 +0300

    vfplus ready for puhti

--- scripts/s0j.sh
+++ scripts/s0j.sh
@@ -2,24 +2,26 @@
 source activate fastvgs
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-data_root="../../../../data"
+data_root=$1
 fb_w2v2_weights_fn="../../../../model/wav2vec_small.pt"
-exp_dir="../../expS2/"
+exp_dir="../../expS0/"
 libri_fn_root="../../../../datavf/ssl6M_root/"
+twd="../../twd/"
 
 python \
 ../run_spokencoco.py \
---subset "subset2" \
+--subset "subset0A" \
 --data_root ${data_root} \
+--trained_weights_dir ${twd} \
 --fb_w2v2_weights_fn ${fb_w2v2_weights_fn} \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
 --batch_size 4 \
---val_batch_size 10 \
---val_cross_batch_size 10 \
---n_epochs 10 \
+--val_batch_size 16 \
+--val_cross_batch_size 64 \
+--n_epochs 200 \
 --n_print_steps 40 \
---n_val_steps 200 \
+--n_val_steps 2000 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --normalize \
