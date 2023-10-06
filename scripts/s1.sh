e6d4aaddac301e02762265df726fd38a2e3498ea
Khazar Khorrami <hxkhkh@mustaotsakolibri.lin.tuni.fi>
2023-10-06 21:22:09 +0300

    fb removed

--- scripts/s1.sh
+++ scripts/s1.sh
@@ -13,15 +13,14 @@ python \
 --subset "subset1" \
 --data_root ${data_root} \
 --trained_weights_dir ${twd} \
---fb_w2v2_weights_fn ${fb_w2v2_weights_fn} \
 --exp_dir ${exp_dir} \
 --libri_fn_root ${libri_fn_root} \
 --batch_size 64 \
 --val_batch_size 64 \
 --val_cross_batch_size 64 \
---n_epochs 200 \
+--n_epochs 100 \
 --n_print_steps 20 \
---n_val_steps 1000 \
+--n_val_steps 100 \
 --lr 0.0001 \
 --warmup_fraction 0.1 \
 --normalize \
