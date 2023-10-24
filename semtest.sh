0f750a73f5cafffd37505a57a43bcc42db6b5a99
khazarkhorrami <khazar.khorrami@gmail.com>
2023-10-24 17:18:26 +0300

    semtest

--- semtest.sh
+++ semtest.sh
@@ -2,29 +2,81 @@
 
 source activate fastvgs
 
+M="RCNN"
+G="exp6M"
 ROOT="/worktmp/khorrami/current"
-GNAME="vfplus"
-MROOT="/worktmp/khorrami/current/FaST/experiments/$GNAME/exp6M"
-mkdir $ROOT/"semtest"/"Smatrix"/$GNAME
-
+MROOT="/worktmp/khorrami/current/FaST/experiments"/$M/$G
+mkdir $ROOT/"semtest/S"/$M/$G
 
 MNAME="expS0"
-SNAME=$GNAME/"S0"
+SNAME=$M/$G/"S0_aL_vO"
 AF="COCO"
 python semRCNN.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF
 
 MNAME="expS1"
-SNAME=$GNAME/"S1"
+SNAME=$M/$G/"S1_aL_vO"
 AF="COCO"
 python semRCNN.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF
 
 MNAME="expS2"
-SNAME=$GNAME/"S2"
+SNAME=$M/$G/"S2_aL_vO"
 AF="COCO"
 python semRCNN.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF
 
 MNAME="expS3"
-SNAME=$GNAME/"S3"
+SNAME=$M/$G/"S3_aL_vO"
+AF="COCO"
+python semRCNN.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF
+
+M="RCNN"
+G="expFB"
+ROOT="/worktmp/khorrami/current"
+MROOT="/worktmp/khorrami/current/FaST/experiments"/$M/$G
+mkdir $ROOT/"semtest/S"/$M/$G
+
+MNAME="expS0"
+SNAME=$M/$G/"S0_aL_vO"
+AF="COCO"
+python semRCNN.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF
+
+MNAME="expS1"
+SNAME=$M/$G/"S1_aL_vO"
+AF="COCO"
+python semRCNN.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF
+
+MNAME="expS2"
+SNAME=$M/$G/"S2_aL_vO"
+AF="COCO"
+python semRCNN.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF
+
+MNAME="expS3"
+SNAME=$M/$G/"S3_aL_vO"
+AF="COCO"
+python semRCNN.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF
+
+M="RCNN"
+G="expR"
+ROOT="/worktmp/khorrami/current"
+MROOT="/worktmp/khorrami/current/FaST/experiments"/$M/$G
+mkdir $ROOT/"semtest/S"/$M/$G
+
+MNAME="expS0"
+SNAME=$M/$G/"S0_aL_vO"
+AF="COCO"
+python semRCNN.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF
+
+MNAME="expS1"
+SNAME=$M/$G/"S1_aL_vO"
+AF="COCO"
+python semRCNN.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF
+
+MNAME="expS2"
+SNAME=$M/$G/"S2_aL_vO"
+AF="COCO"
+python semRCNN.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF
+
+MNAME="expS3"
+SNAME=$M/$G/"S3_aL_vO"
 AF="COCO"
 python semRCNN.py --exp_dir $MROOT/$MNAME --root $ROOT --Sname $SNAME --afiles $AF
 
